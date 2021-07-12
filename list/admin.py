2f39f2fb581cf19e191dd669dda0e8d1896cb3f3
bishi <a414510nee@yahoo.co.jp>
2021-07-12 17:41:11 +0900

    デプロイ完了

---
+++ list/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import SlotData
+
+admin.site.register(SlotData)
---
+++ list/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from datetime import datetime
+
+
+class SlotData(models.Model):
+    class Meta:
+        db_table = 'data'
+        verbose_name ="データ一覧"
+        verbose_name_plural = 'データ一覧'
+
+    store_name = models.CharField(max_length=100, verbose_name='店舗名')
+    name = models.CharField(max_length=100, verbose_name='機種名')
+    number = models.IntegerField(verbose_name='台番号')
+    date = models.DateField(verbose_name='日付', default=datetime.now())
+    bigbonus = models.IntegerField(verbose_name='BB')
+    regularbonus = models.IntegerField(verbose_name='RB')
+    count =models.IntegerField(verbose_name='総回転数')
+    payout = models.IntegerField(verbose_name='差枚数')
+    memo = models.CharField(max_length=50, verbose_name='メモ', blank=True, null=True)
+
+    def __str__(self):
+       return self.name
