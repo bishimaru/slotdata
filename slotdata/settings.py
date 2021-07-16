65b37429cc63590eab963806af2f1f1545db6e4c
bishimaru <a414510nee@yahoo.co.jp>
2021-07-16 20:24:52 +0900

    database更新

--- slotdata/settings.py
+++ slotdata/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'slotdata.wsgi.application'
 
 DATABASES = {
 
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'slotdata',
-    #     'USER': 'bishi',
-    #     'PASSWORD': '7234',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    #     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'slotdata',
+        'USER': 'bishi',
+        'PASSWORD': '7234',
+        'HOST': 'localhost',
+        'PORT': '',
+        }
 }
 
 
